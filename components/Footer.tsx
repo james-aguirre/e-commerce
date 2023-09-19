d47854443c655e84ca2fc3e05da317733e064a0c
James Aguirre <jamesaguirre98@gmail.com>
2023-09-18 23:23:08 -0700

    footer

---
+++ components/Footer.tsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <footer className="bg-white border-t">
+      <div className="mx-auto py-10">
+        <p className="text-center text-gray-800 text-xs">&copy; James Aguirre, 2023</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
