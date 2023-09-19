1f09914a1ec6dd08fcc46b256043047fbaa7fd85
James Aguirre <jamesaguirre98@gmail.com>
2023-09-19 00:11:06 -0700

    supabase env

--- components/Main-Nav.tsx
+++ components/Main-Nav.tsx
@@ -1,8 +1,12 @@
 "use client";
 
 import { usePathname } from "next/navigation";
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+import { Category } from "@/public/types";
+
 interface MainNavProps {
-  data: any;
+  data: Category;
 }
 
 const MainNav: React.FC<MainNavProps> = ({ data }) => {
@@ -12,7 +16,22 @@ const MainNav: React.FC<MainNavProps> = ({ data }) => {
     label: route.name,
     active: pathname === `/category/${route.id}`,
   }));
-  return <nav>main nav</nav>;
+  return (
+    <nav className="mx-6 flex items-center space-x-4 lg:space-x-6">
+      {routes.map((route) => (
+        <Link
+          key={route.href}
+          href={route.href}
+          className={cn(
+            "text-sm font-medium transition-colors hover:text-white",
+            route.active ? "text-white" : "text-gray-300"
+          )}
+        >
+          {route.label}
+        </Link>
+      ))}
+    </nav>
+  );
 };
 
 export default MainNav;
